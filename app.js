943110e4eb5c9138f2a69b868c85dca9d6c5a78b
Karl Gregor Rauniste <karlgregorrauniste@Karls-MBP.lan>
2020-04-23 19:14:17 +0300

    changes

--- app.js
+++ app.js
@@ -23,3 +23,20 @@ fetch("./data.json")
         var output = document.getElementById('question');
         output.innerHTML += data.Questions[0].question + '<br>';
     })
+
+
+
+const realFileBtn = Document.getElementById("real-file");
+const customBtn = Document.getElementById("custom-text");
+const customTxt = Document.getElementById("button1");
+customBtn.addEventListener("click", function() {
+    realFileBtn.click();
+});
+
+realFileBtn.addEventListener("change", function() {
+    if (realFileBtn.value) {
+        customTxt.innerHTML = realFileBtn.value.match();
+    } else {
+        customTxt.innerHTML = "No file chosen, yet ";
+    }
+});
